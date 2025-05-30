1c8d994e3de6f09a9499c5ec160d24c6ae629126
sedazeyy <sedazeyx@gmail.com>
2025-05-30 13:21:32 +0300

    Add files via upload

---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,14 @@
+#string: karakter dizisi
+#saymaya 0dan başlanır köşeli parantez kullanılır.- verilirse geriden sayılır.
+#large integers -büyük tam sayılar- için araya , koyulur
+#float: ondalıklı sayı
+#boolean: iki değer döndürür. True(T) or False(F)
+print("Hello"[0])
+print("123" + "345")
+print(123_456_789)
+print(3.14159)
+print(True)
+print(False)
+#string: çift tırnak içindeki her şey
+#integer: tam sayı
+
