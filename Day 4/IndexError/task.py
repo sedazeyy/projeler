a7ea8fd4a26b25fc2bbd761b1ea4f6cb7e9618c9
sedazeyy <sedazeyx@gmail.com>
2025-05-30 13:22:57 +0300

    Add files via upload

---
+++ Day 4/IndexError/task.py
@@ -0,0 +1,8 @@
+# dirty_dozen = ["Strawberries", "Nectarines", "Spinach", "Apple", "Pear", "Peaches", "Cherry" "Grapes"]
+
+fruits = ["Strawberries","Nectarines","Apples","Grapes","Peaches","Cherries","Pear"]
+vegetables = ["Spinach","Kale","Tomatoes","Celery","Potatoes"]
+
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
